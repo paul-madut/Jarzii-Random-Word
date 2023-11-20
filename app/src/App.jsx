7eb9660a3dd1c974478b83704a91d03e9aa151a0
Paul Madut <paulmadut@mths.ca>
2023-11-20 00:40:44 -0500

    tailwindcss setup

---
+++ app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen w-full bg-blue-500'>
+        <h1 className=' text-xl text-gray-950'>Text</h1>
+        <p>Helloo</p>
+      </div>
+    </>
+  )
+}
+
+export default App
